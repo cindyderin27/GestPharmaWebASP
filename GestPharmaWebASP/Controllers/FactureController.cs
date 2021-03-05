dc9d2b10040d0c5b2ca5ce988d712d0be0fe6829
cindyderin27 <61082408+cindyderin27@users.noreply.github.com>
2021-03-05 17:46:47 +0100

    ajout des dossiers

---
+++ GestPharmaWebASP/Controllers/FactureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestPharmaWebASP.Controllers
+{
+    public class FactureController : Controller
+    {
+        // GET: Facture
+        public ActionResult Facture()
+        {
+            return View("FactureView");
+        }
+    }
+}
